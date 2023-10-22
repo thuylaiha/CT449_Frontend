a366aea53b3509cfa7e15338054820e1c8498c60
thuylaiha <125327820+thuylaiha@users.noreply.github.com>
2023-10-22 14:00:25 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,16 @@ path: "/",
 name: "contactbook",
 component: ContactBook,
 },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+},
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
 });
+
 export default router;
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            OOps, không thể tìm thấy trang. Trở về
+            <router-link to="/">Trang chủ.</router-link>
+        </p>
+    </div>
+</template>
